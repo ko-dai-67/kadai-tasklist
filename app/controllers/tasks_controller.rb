28cccc237aaa0e6d8c45fd53bc14c92ee7121cc3
kodai.iwashita <back.together0607@gmail.com>
2021-02-13 01:18:54 +0000

    pogram-delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :destroy, :update]
+  before_action :correct_user, only: [:show, :destroy, :update, :edit]
   def index
       @tasks = current_user.tasks
   end
@@ -45,10 +44,6 @@ class TasksController < ApplicationController
   end 
   
  private
-
- def set_task
-  @task = Task.find(params[:id])
- end
 
  def task_params
   params.require(:task).permit(:status, :content)
